f5ba0821b04a46df039c23ea649b4cbbd8d42765
Huraira Imtiaz <huraira0707392@gmail.com>
2022-08-01 10:34:44 +0500

    practicing variables

---
+++ index.js
@@ -0,0 +1,4 @@
+var user = "huraira";
+console.log(user);
+var message = "Hello World Hurairaimriaz here!";
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,8 @@
+let user = "huraira";
+console.log(user);
+var message = "Hello World Hurairaimtiaz here!"
+console.log(message);
+var number = 34;
+var gender : boolean = true;
+console.log (number);
+console.log (gender);
