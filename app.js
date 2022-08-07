8ef7d040d9713eb66364103a637f361ed714aa8f
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2022-08-07 23:59:01 +0500

    relational operators

---
+++ app.js
@@ -0,0 +1,25 @@
+var number1 = 700;
+var number2 = 800;
+console.log("your number is =", number1);
+console.log("your number is =", number2);
+var Name = "Huraira Imtiaz";
+console.log("your name is :", Name);
+//------------------- relational operators-----------------------------
+// 1) less than operator
+var result1 = number1 < number2;
+console.log("your result1 is :", result1);
+// 2) greater than operator
+var result2 = number1 > number2;
+console.log("your result2 is :", result2);
+// 3) less-than-equal to operator
+var result3 = number1 <= number2;
+console.log("your result3 is :", result3);
+// 4) greater-than-eqaul to  operator
+var result4 = number1 >= number2;
+console.log("your result4 is :", result4);
+// 4) eqaul to  operator
+var result5 = number1 == number2;
+console.log("your result5 is :", result5);
+// 4) not-eqaul to  operator
+var result6 = number1 != number2;
+console.log("your result6 is :", result6);
