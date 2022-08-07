3e991b21df891aec5524c95d223ebae366af27d4
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2022-08-07 23:59:59 +0500

    logical operators and type of operator

--- app.js
+++ app.js
@@ -23,3 +23,15 @@ console.log("your result5 is :", result5);
 // 4) not-eqaul to  operator
 var result6 = number1 != number2;
 console.log("your result6 is :", result6);
+//------------------- logical operators-----------------------------
+// 1) AND OERATOR
+var logicalOperator = number1 == number2 && number1 != number2;
+console.log("your result of logical operator AND is :", logicalOperator);
+// 1) OR OERATOR
+var logicalOperator = number1 == number2 || number1 != number2;
+console.log("your result of logical operator OR is :", logicalOperator);
+//-------------------type of----------------------------
+console.log("tyhe type of your variable is :", typeof number1);
+console.log("tyhe type of your variable is :", typeof result1);
+console.log("tyhe type of your variable is :", typeof logicalOperator);
+console.log("tyhe type of your variable is :", typeof Name);
