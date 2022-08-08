f8e3c6acebe6726bb5627240f7077e7b9a239f84
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2022-08-08 20:40:26 +0500

    program 1`
    It displays the square of the given number. (but don't input from the user)

---
+++ program1.js
@@ -0,0 +1,7 @@
+var Name = "I am Program 1";
+console.log(Name);
+// a program that inputs a number and display its squre 
+var number = 5;
+console.log("Enter your number here=", number);
+var square = 5 * 5;
+console.log("Square of your given number is=", square);
---
+++ program1.ts
@@ -0,0 +1,10 @@
+var Name:string="I am Program 1";
+console.log(Name);
+
+
+// a program that inputs a number and display its squre 
+
+var number:number= 5;
+console.log("Enter your number here=" , number);
+var square:any= 5*5;
+console.log("Square of your given number is=" , square);
