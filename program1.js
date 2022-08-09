f231e1aa4cd280e19ef8215b1a3a5dd18d2379ec
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2022-08-09 22:45:17 +0500

    sum program
    it display the sum of 2 numbers

---
+++ program1.js
@@ -0,0 +1,12 @@
+var Name = "I am Program 1";
+console.log(Name);
+// a program that inputs a number and display its squre 
+var number = 5;
+console.log("Enter your number here=", number);
+var square = 5 * 5;
+console.log("Square of your given number is=", square);
+// a program that sum two numbers
+var number1 = 2;
+var number2 = 4;
+var sum = number1 + number2;
+console.log("the sum of numbers is =", sum);
---
+++ program1.ts
@@ -0,0 +1,17 @@
+var Name:string="I am Program 1";
+console.log(Name);
+
+
+// a program that inputs a number and display its squre 
+
+var number:number= 5;
+console.log("Enter your number here=" , number);
+var square:any= 5*5;
+console.log("Square of your given number is=" , square);
+
+// a program that sum two numbers
+
+var number1:number=2;
+var number2:number=4;
+var sum:number= number1+number2;
+console.log("the sum of numbers is =",sum);
